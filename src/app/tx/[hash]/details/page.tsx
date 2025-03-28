6a09d90f8df4ec5fecddf77d0e5eb90c471d5492
VladChernenko <vladchernenko@protonmail.com>
2025-03-28 15:29:23 +0200

    Format docs

--- src/app/tx/[hash]/details/page.tsx
+++ src/app/tx/[hash]/details/page.tsx
@@ -1,36 +1,37 @@
-import { Metadata } from 'next';
-import { Grid, Typography } from '@mui/material';
-import { ContentBlock, PageContainer } from '@/components/ui';
-import { PrettyJSON } from '@/components';
-import { fetchTransactionByTxHash } from '@/data';
+import { Metadata } from "next";
+import { Grid, Typography } from "@mui/material";
+import { ContentBlock, PageContainer } from "@/components/ui";
+import { PrettyJSON } from "@/components";
+import { fetchTransactionByTxHash } from "@/data";
 
 interface RawTransactionWithDetailsProps {
   params: {
     hash: string;
-  }
+  };
 }
 
 export const metadata: Metadata = {
-  title: 'Raw tx details',
+  title: "Raw tx details",
 };
 
-export default async function RawTransactionWithDetailsPage({ params }: RawTransactionWithDetailsProps) {
+export default async function RawTransactionWithDetailsPage({
+  params,
+}: RawTransactionWithDetailsProps) {
   const id = decodeURIComponent(params.hash);
   const tx = await fetchTransactionByTxHash(id);
 
   const detailsToVisualize = {
     executionStatus: {
       isOk: tx.isOk,
-      reason:
-      tx.reason,
+      reason: tx.reason,
       blockID: tx.blockID,
       order: tx.order,
       createdContractAddress: tx.createdContractAddress,
       extraDataToReceipt: tx.extraDataToReceipt,
       priorityFee: tx.priorityFee,
-      totalFee: tx.totalFee
+      totalFee: tx.totalFee,
     },
-    txData:{
+    txData: {
       version: tx.v,
       shard: tx.shard,
       txid: tx.txHash,
@@ -40,17 +41,17 @@ export default async function RawTransactionWithDetailsPage({ params }: RawTrans
       nonce: tx.nonce,
       sigType: tx.sigType,
       sig: tx.sig,
-      payload: tx.payload
-    }
-  }
+      payload: tx.payload,
+    },
+  };
 
   return (
     <PageContainer sx={{ py: 6 }}>
-      <Typography variant='h1'>Raw transaction with details</Typography>
+      <Typography variant="h1">Raw transaction with details</Typography>
       <Grid container spacing={1} sx={{ mt: 2 }}>
         <Grid item xs={12}>
           <ContentBlock
-            title='Transaction ID:'
+            title="Transaction ID:"
             value={tx.txHash}
             url={`/tx/${tx.txHash}`}
           />
@@ -61,19 +62,27 @@ export default async function RawTransactionWithDetailsPage({ params }: RawTrans
           </ContentBlock>
         </Grid>
       </Grid>
-      <Typography mt={8} variant='h1'>State changes</Typography>
+      <Typography mt={8} variant="h1">
+        State changes
+      </Typography>
       <Grid container spacing={1} sx={{ mt: 2 }}>
         <Grid item xs={12}>
           <ContentBlock>
-            <Typography textAlign={'center'} sx={{ mt: 3 }}>This will be available later</Typography>
+            <Typography textAlign={"center"} sx={{ mt: 3 }}>
+              This will be available later
+            </Typography>
           </ContentBlock>
         </Grid>
       </Grid>
-      <Typography mt={8} variant='h1'>Events log</Typography>
+      <Typography mt={8} variant="h1">
+        Events log
+      </Typography>
       <Grid container spacing={1} sx={{ mt: 2 }}>
         <Grid item xs={12}>
           <ContentBlock>
-            <Typography textAlign={'center'} sx={{ mt: 3 }}>This will be available later</Typography>
+            <Typography textAlign={"center"} sx={{ mt: 3 }}>
+              This will be available later
+            </Typography>
           </ContentBlock>
         </Grid>
       </Grid>
--- src/app/users/[id]/page.tsx
+++ src/app/users/[id]/page.tsx
@@ -1,47 +1,58 @@
-'use client';
+"use client";
 
-import React, { useState, useEffect } from 'react';
-import { Metadata } from 'next';
-import NotFoundPage from '@/app/not-found';
-import { Box, Typography, Tabs, Tab } from '@mui/material';
-import { ContentBlock, EntityPageLayout, Label, PageContainer, TransactionsTable } from '@/components/ui';
-import { fetchAccountById, fetchAccountTransactions } from '@/data';
-import { truncateMiddle } from '@/helpers';
-import { TransactionPreview, UserAccount } from '@/definitions';
-import AccountImage from '@public/icons/pages/account.svg';
-import Web3 from 'web3';
+import React, { useState, useEffect } from "react";
+import { Metadata } from "next";
+import NotFoundPage from "@/app/not-found";
+import { Box, Typography, Tabs, Tab } from "@mui/material";
+import {
+  ContentBlock,
+  EntityPageLayout,
+  Label,
+  PageContainer,
+  TransactionsTable,
+} from "@/components/ui";
+import { fetchAccountById, fetchAccountTransactions } from "@/data";
+import { truncateMiddle } from "@/helpers";
+import { TransactionPreview, UserAccount } from "@/definitions";
+import AccountImage from "@public/icons/pages/account.svg";
+import Web3 from "web3";
 
 const metadata: Metadata = {
-  title: 'Account info',
+  title: "Account info",
 };
 
 interface Props {
   params: {
-    id: string
-  }
+    id: string;
+  };
 }
 
 export default function AccountByIdPage({ params }: Props) {
   const [account, setAccount] = useState<UserAccount | null>(null);
   const [transactions, setTransactions] = useState<TransactionPreview[]>([]);
   const [loading, setLoading] = useState(true);
-  const [shard, setShard] = useState('');
-  const [accountId, setAccountId] = useState('');
+  const [shard, setShard] = useState("");
+  const [accountId, setAccountId] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
-      let [shard, accountId] = decodeURIComponent(params.id).split(':');
-      accountId = accountId.startsWith('0x') ? accountId.toLowerCase() : accountId;
+      let [shard, accountId] = decodeURIComponent(params.id).split(":");
+      accountId = accountId.startsWith("0x")
+        ? accountId.toLowerCase()
+        : accountId;
       setShard(shard);
       setAccountId(accountId);
 
       try {
         const accountData = await fetchAccountById(shard, accountId);
-        const transactionsData = await fetchAccountTransactions(shard, accountId);
+        const transactionsData = await fetchAccountTransactions(
+          shard,
+          accountId
+        );
         setAccount(accountData as UserAccount);
         setTransactions(transactionsData);
       } catch (error) {
-        console.error('Error fetching account data:', error);
+        console.error("Error fetching account data:", error);
       } finally {
         setLoading(false);
       }
@@ -52,13 +63,20 @@ export default function AccountByIdPage({ params }: Props) {
 
   if (loading) {
     return (
-      <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
         <Typography>Loading...</Typography>
       </Box>
     );
   }
 
-  if (!account || account.type !== 'eoa') {
+  if (!account || account.type !== "eoa") {
     return <NotFoundPage />;
   }
 
@@ -66,30 +84,42 @@ export default function AccountByIdPage({ params }: Props) {
     <PageContainer sx={{ py: 6 }}>
       <EntityPageLayout
         header={{
-          title: 'Account info',
+          title: "Account info",
           clipBoardValue: accountId,
           value: truncateMiddle(accountId),
           label: {
-            variant: 'green',
-            value: 'User'
-          }
+            variant: "green",
+            value: "User",
+          },
         }}
         items={[
-          <ContentBlock key='account_id' title='Account Id:' value={accountId} />,
-          <ContentBlock key='aliases' title='Also known as:'>
-            <Label variant='blue'>N/A</Label>
+          <ContentBlock
+            key="account_id"
+            title="Account Id:"
+            value={accountId}
+          />,
+          <ContentBlock key="aliases" title="Also known as:">
+            <Label variant="blue">N/A</Label>
           </ContentBlock>,
           [
-            <ContentBlock key='shard' title='Shard:' value={shard} />,
-            <ContentBlock key='balance' title='Balance:' value={Web3.utils.fromWei(account.balance, 'ether') + ' KLY'} />
+            <ContentBlock key="shard" title="Shard:" value={shard} />,
+            <ContentBlock
+              key="balance"
+              title="Balance:"
+              value={Web3.utils.fromWei(account.balance, "ether") + " KLY"}
+            />,
           ],
           [
-            <ContentBlock key='nonce' title='Nonce:' value={account.nonce} />,
-            <ContentBlock key='abstract_gas' title='Abstract gas:' value={account.gas} />
-          ]
+            <ContentBlock key="nonce" title="Nonce:" value={account.nonce} />,
+            <ContentBlock
+              key="abstract_gas"
+              title="Abstract gas:"
+              value={account.gas}
+            />,
+          ],
         ]}
       >
-        <AccountImage width={421} height={426} viewBox='0 0 421 426' />
+        <AccountImage width={421} height={426} viewBox="0 0 421 426" />
       </EntityPageLayout>
 
       <TabSection transactions={transactions} />
@@ -101,29 +131,35 @@ function TabSection({ transactions }: { transactions: TransactionPreview[] }) {
   const [activeTab, setActiveTab] = useState(0);
 
   return (
-<Box sx={{ mt: 10, overflowX: 'auto', whiteSpace: 'nowrap' }}>
-  <Tabs
-    sx={{ mb: 10, minWidth: 'max-content' }}
-    value={activeTab}
-    onChange={(_, newIndex) => setActiveTab(newIndex)}
-  >
-        <Tab label='Transactions' />
-        <Tab label='Staking data' />
-        <Tab label='Portfolio' />
+    <Box sx={{ mt: 10, overflowX: "auto", whiteSpace: "nowrap" }}>
+      <Tabs
+        sx={{ mb: 10, minWidth: "max-content" }}
+        value={activeTab}
+        onChange={(_, newIndex) => setActiveTab(newIndex)}
+      >
+        <Tab label="Transactions" />
+        <Tab label="Staking data" />
+        <Tab label="Portfolio" />
       </Tabs>
 
       {activeTab === 0 && (
         <>
-          <Typography variant='h1'>Transactions</Typography>
-          <Typography sx={{ mt: 1, mb: 3 }}>Browse through the latest 200 transactions below</Typography>
+          <Typography variant="h1">Transactions</Typography>
+          <Typography sx={{ mt: 1, mb: 3 }}>
+            Browse through the latest 200 transactions below
+          </Typography>
           <TransactionsTable transactions={transactions.reverse()} />
         </>
       )}
       {activeTab === 1 && (
-        <Typography textAlign={'center'} sx={{ mt: 3 }}>This will be available later</Typography>
+        <Typography textAlign={"center"} sx={{ mt: 3 }}>
+          This will be available later
+        </Typography>
       )}
       {activeTab === 2 && (
-        <Typography textAlign={'center'} sx={{ mt: 3 }}>This will be available later</Typography>
+        <Typography textAlign={"center"} sx={{ mt: 3 }}>
+          This will be available later
+        </Typography>
       )}
     </Box>
   );
